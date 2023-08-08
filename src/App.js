6d188040152667a5c5eb681d372d10db0f602c5a
Ainerukundo Edwins <ainedwins@gmail.com>
2023-08-08 16:08:52 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [profileData, setProfileData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (response.ok) {
+        const data = await response.json();
+        setProfileData(data);
+      } else {
+        setProfileData(null);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Profile Search</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {profileData && (
+        <div className="profile-container">
+          <img src={profileData.avatar_url} alt="Profile" />
+          <h2>{profileData.login}</h2>
+          <p>{profileData.bio}</p>
+          <div className="details">
+            <p><strong>Followers:</strong> {profileData.followers}</p>
+            <p><strong>Following:</strong> {profileData.following}</p>
+            <p><strong>Repositories:</strong> {profileData.public_repos}</p>
+            {profileData.location && <p><strong>Location:</strong> {profileData.location}</p>}
+          </div>
+          <a href={profileData.html_url} target="_blank" rel="noopener noreferrer">
+            View Profile
+          </a>
+        </div>
+      )}
     </div>
   );
 }
